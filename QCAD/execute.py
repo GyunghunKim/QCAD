04eedb8d4f354353983200f7a562cfab324fb250
Gyunghun Kim <ghuni@snu.ac.kr>
2018-09-03 23:05:33 +0900

    ADD Package Backend ADD Fucntion execute ADD Module MatrixModel DEL Class Gate MODIFIED Class Module

--- QCAD/execute.py
+++ QCAD/execute.py
@@ -6,15 +6,12 @@ Date:   2018.09.01
 Function execute connects between front-end and back-end.
 """
 
+from . import QuantumCircuit
 from . import Backend
 
 
-def execute(quantum_circuit, backend):
-    """
-    :param quantum_circuit: 게이트와 레지스터가 정의된 QuantumCircuit 객체
-    :param backend: backend의 타입을 string 형태로 명시
-    :return: Result 객체를 리턴
-    """
+def execute(quantum_circuit: QuantumCircuit, backend):
+    # TODO: 무엇을 리턴할 지 생각해 보아야 함.
 
     # No Optimized Matrix Model
     if backend is "NOMM":
