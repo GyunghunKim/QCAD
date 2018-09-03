0531b0e6c21b48db0d81f96d6020d4114a7a7398
Gyunghun Kim <ghuni@snu.ac.kr>
2018-09-03 19:18:59 +0900

    CHANGE 인덱스 상위 모듈에 관점에서 고려된 것으로 바꿈

--- QCAD/module.py
+++ QCAD/module.py
@@ -1,5 +1,6 @@
 class Module(object):
     def __init__(self, name, n, ind_modules=[]):
+        # initialize
         self.name = name
         self.n = n
         self.sub_modules = []
@@ -10,47 +11,49 @@ class Module(object):
             self.sub_modules.append(_ind_module[0])
             self.reg_indices.append(_ind_module[1])
             for _ind_module1 in range(len(_ind_module[1])):
-                if(_ind_module[1][_ind_module1]>=self.n):
+                if not _ind_module[1][_ind_module1] < self.n:
                     raise IndexError
-
 
     def set_typical(self):
         self.typical=True
 
-
     def __getitem__(self, item):
         if isinstance(item, int):
-            if self.n!=1:
+            if self.n is not 1:
                 raise RegistermatchError()
-            return [self, (item,)]
+            return [self, [item]]
 
-        if len(item)!=self.n:
+        if len(item) is not self.n:
             raise RegistermatchError()
 
         return [self, item]
 
     def get_submodules(self):
         #모듈의 sub_modules의 정보를 얻는 함수
-        return self.sub_modules , self.reg_indices
+        return self.sub_modules, self.reg_indices
 
     def typ_decompose(self):
         # 모듈을 기본적인 모듈(게이트)로 분해해주는 함수
-        temp_sub_modules=self.sub_modules
-        temp_reg_indices=self.reg_indices
+        temp_sub_modules = self.sub_modules.copy()
+        temp_reg_indices = self.reg_indices.copy()
 
-        for i in range(len(temp_sub_modules)):
+        for i in range(len(self.reg_indices)):
+            if temp_sub_modules[i].typical is False:
+                temp_decom = temp_sub_modules[i].typ_decompose()
 
-              if(temp_sub_modules[i].typical==False):
-                  temp_decom=temp_sub_modules[i].typ_decompose()
+                del temp_sub_modules[i]
+                for j in range(len(temp_decom[0])):
+                    temp_sub_modules.insert(i+j, temp_decom[0][j])
 
-                  del temp_sub_modules[i]
-                  for j in range(len(temp_decom[0])):
-                      temp_sub_modules.insert(i+j,temp_decom[0][j])
-                  del temp_reg_indices[i]
-                  for k in range(len(temp_decom[1])):
-                     temp_reg_indices.insert(i + k, temp_decom[1][k])
+                del temp_reg_indices[i]
+                for k in range(len(temp_decom[1])):
+                    temp_indecies = []
+                    for l in temp_decom[1][k]:
+                        temp_indecies.append(self.reg_indices[i][l])
+                    temp_reg_indices.insert(i + k, temp_indecies)
 
-        return temp_sub_modules,temp_reg_indices
+        return temp_sub_modules, temp_reg_indices
+
 
 class TypicalModule:
     I = Module('I', 1)
@@ -59,13 +62,17 @@ class TypicalModule:
     Y = Module('Y', 1)
     Z = Module('Z', 1)
     CZ = Module('CZ', 2)
+
     I.set_typical()
     H.set_typical()
     X.set_typical()
     Y.set_typical()
     Z.set_typical()
     CZ.set_typical()
+
     def __init__(self):
         pass
+
+
 class RegistermatchError(Exception):
     pass
