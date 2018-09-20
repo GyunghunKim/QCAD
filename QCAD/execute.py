a5b7f4d7229227cc5cb5c6e99d228e9063162131
Gyunghun Kim <ghuni@snu.ac.kr>
2018-09-21 07:33:57 +0900

    ADD Barplot

--- QCAD/execute.py
+++ QCAD/execute.py
@@ -4,6 +4,7 @@ from . import QuantumCircuit
 from . import Backend
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def execute(backend, quantum_circuit: QuantumCircuit, initial_state = []):
     # TODO: 무엇을 리턴할 지 생각해 보아야 함.
@@ -12,4 +13,11 @@ def execute(backend, quantum_circuit: QuantumCircuit, initial_state = []):
     if backend is "MatrixModel":
         _res = Backend.MatrixModel.run(quantum_circuit, initial_state)
         print(_res)
-        print([x*np.conjugate(x) for x in _res])
+
+        print([(x*np.conjugate(x))[0] for x in _res])
+        plt.bar(np.arange(2 ** quantum_circuit.n), [(x*np.conjugate(x))[0] for x in _res])
+        plt.ylabel('Probability')
+        plt.xticks(np.arange(0, 2 ** quantum_circuit.n))
+        plt.ylim(bottom=0, top=1)
+
+        plt.show()
