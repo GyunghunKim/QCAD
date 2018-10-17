7e5b44a4b497aebe8634c865418c85bac080f7ea
Gyunghun Kim <ghuni@snu.ac.kr>
2018-10-17 19:32:37 +0900

    Fix quantum_circuit

--- QCAD/__init__.py
+++ QCAD/__init__.py
@@ -3,4 +3,6 @@ from .module import TypicalModule
 
 from .qunautm_circuit import QuantumCircuit
 
+from .qcad2qasm import qcad2qasm
+
 from .execute import execute
--- QCAD/qcad2qasm.py
+++ QCAD/qcad2qasm.py
@@ -1,28 +1,35 @@
 from . import QuantumCircuit
 from . import Module
 
-def qcad2qasm(qc:QuantumCircuit, option="IBM_QASM", decomposed=True):
-    qc_typ_sub_modules=qc.module.typ_decompose()[0].copy()
-    qc_typ_reg_indices=qc.module.typ_decompose()[1].copy()
+def qcad2qasm(qc: QuantumCircuit, option='IBM_QASM', decomposed=True, to_file=True):
+    output = ''
+    
+    qc_typ_sub_modules = qc.module.typ_decompose()[0].copy()
+    qc_typ_reg_indices = qc.module.typ_decompose()[1].copy()
 
-    if(option is "IBM_QASM"):
-        f = open(qc.module.name+"_qasm.txt", 'w')
-        f.write("OPENQASM 2.0;\ninclude \"qelib1.inc\";\n ")
-        wqcbit = "qreg q[%d];\ncreg c[%d];\n" % (qc.n,qc.n)
-        f.write(wqcbit)
-        
-        modulenum=0
-        for typ_sub_module in qc_typ_sub_modules:
+    print(qc_typ_reg_indices);
+    print(qc_typ_sub_modules);
+
+    if option is 'IBM_QASM':
+        output += 'OPENQASM 2.0;\ninclude \"qelib1.inc\";\n\n'
+        output += 'qreg q[%d];\ncreg c[%d];\n\n' % (qc.n, qc.n)
+
+        modulenum = 0
+        for module_num, typ_sub_module in zip(range(len(qc_typ_sub_modules)), qc_typ_sub_modules):
             module_name = typ_sub_module.name.lower()
-            data = module_name
-            indnum=0
-            for reg_indices in qc_typ_reg_indices[modulenum]:
-                if(indnum is not 0):
-                    data += ", "
-                data += "q[%d]" % reg_indices[indnum]
-                indnum += 1 
-            modulenum += 1
-        data += ";\n"
-        f.write(data)
+            output += module_name + ' '
+            indnum = 0
+            for reg_index in qc_typ_reg_indices[module_num]:
+                output += 'q[%d], ' % reg_index
+                indnum += 1
+            output += '\b\b;\n'
+        
+        for i in range(qc.n):
+            output += 'measure q[%d]->c[%d];\n' % (i, i)
 
-
+    if to_file:
+        f = open(qc.module.name+'_qasm.txt', 'w')
+        f.write(output)
+        f.close()
+    else:
+        return output
